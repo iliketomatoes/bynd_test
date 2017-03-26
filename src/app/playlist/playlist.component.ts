1b8cc5a3f81571dc8d3a68c280d8b811b9c749d1
Giancarlo Soverini <giancarlosoverini@gmail.com>
2017-03-26 00:37:15 -0400

    feat: Implement pagination

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { VideoService } from '../video/';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/switchMap';
@@ -14,10 +14,17 @@ export class PlaylistComponent implements OnInit, OnDestroy {
 	public playlist: any[];
 	public page: any[];
 	public pages: number[];
+	public isFirstPage = true;
+	public isLastPage = false;
 
 	private routeSubscription: ISubscription;
+	private currentPage: number;
 
-	constructor(public videoService: VideoService, private route: ActivatedRoute) { }
+	constructor(
+		public videoService: VideoService,
+		private route: ActivatedRoute,
+		private router: Router
+	) { }
 
 	public ngOnInit(): void {
 
@@ -32,6 +39,7 @@ export class PlaylistComponent implements OnInit, OnDestroy {
 						return Observable.of(params);
 					})
 					.subscribe((param) => {
+						this.currentPage = +param.page;
 						this.populatePage(+param.page);
 					});
 			},
@@ -45,8 +53,34 @@ export class PlaylistComponent implements OnInit, OnDestroy {
 		this.routeSubscription.unsubscribe();
 	}
 
+	public goToNext() {
+		if (!this.isLastPage) {
+			let nextPage = (this.currentPage) + 1;
+			this.router.navigate(['/playlist', nextPage]);
+		}
+	}
+
+	public goToPrev() {
+		if (!this.isFirstPage) {
+			let prevPage = this.currentPage - 1;
+			this.router.navigate(['/playlist', prevPage]);
+		}
+	}
+
 	private populatePage(page: number) {
 		let index = (page - 1) * 5;
 		this.page = this.playlist.slice(index, (index + 5));
+		if (page === 1) {
+			this.isFirstPage = true;
+			this.isLastPage = false;
+		} else {
+			if (page === this.pages.length) {
+				this.isFirstPage = false;
+				this.isLastPage = true;
+			} else {
+				this.isFirstPage = false;
+				this.isLastPage = false;
+			}
+		}
 	}
 }
