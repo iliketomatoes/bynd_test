2cb72aeec24675e6cf09254242e0743ac9e39633
Giancarlo Soverini <giancarlosoverini@gmail.com>
2017-03-26 09:55:35 -0400

    docs: Add some comments in playlist component

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -11,13 +11,27 @@ import { ISubscription } from 'rxjs/Subscription';
 })
 export class PlaylistComponent implements OnInit, OnDestroy {
 
+	// Array containing all videos
 	public playlist: any[];
+
+	// Array containing only visible videos
 	public page: any[];
+
+	/**
+	 * Dumb helper array needed for the the pagination,
+	 * since *ngFor loop needs an iterable object to loop through.
+	 * That means a simple number is not enough.
+	 * The length of this array is equal to the number of total pages.
+	 */
 	public pages: number[];
+
+	// Helpers variables needed in the view for "prev" and "next" pagination buttons
 	public isFirstPage = true;
 	public isLastPage = false;
 
 	private routeSubscription: ISubscription;
+
+	// Index of current page
 	private currentPage: number;
 
 	constructor(
@@ -28,12 +42,13 @@ export class PlaylistComponent implements OnInit, OnDestroy {
 
 	public ngOnInit(): void {
 
+		// Get the videos
 		this.videoService.getVideos().subscribe(
 			(videos) => {
-				console.log(videos[0]);
 				this.playlist = videos;
 				this.pages = Array(Math.round(videos.length / 5));
 
+				// Get the param from the route in order to know what page to show
 				this.routeSubscription = this.route.params
 					.switchMap((params: Params) => {
 						return Observable.of(params);
@@ -69,7 +84,11 @@ export class PlaylistComponent implements OnInit, OnDestroy {
 
 	private populatePage(page: number) {
 		let index = (page - 1) * 5;
+
+		// Get the videos to show
 		this.page = this.playlist.slice(index, (index + 5));
+
+		// Update helpers variables for "next" and "prev" pagination buttons
 		if (page === 1) {
 			this.isFirstPage = true;
 			this.isLastPage = false;
