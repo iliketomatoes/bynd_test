6bef9628ecb76e75638d25f06766cfe9c86cff54
Giancarlo Soverini <giancarlosoverini@gmail.com>
2017-03-26 20:49:00 -0400

    chore: Update some favicons

--- config/head-config.common.js
+++ config/head-config.common.js
@@ -9,25 +9,23 @@
  * { name: 'msapplication-TileImage', content: '/assets/icon/ms-icon-144x144.png', '=content': true },
  * Will prefix the publicPath to content.
  *
- * { rel: 'apple-touch-icon', sizes: '57x57', href: '/assets/icon/apple-icon-57x57.png', '=href': false },
+ * { rel: 'apple-touch-icon', sizes: '57x57', href: '/assets/icon/apple-touch-icon-57x57.png', '=href': false },
  * Will not prefix the publicPath on href (href attributes are added by default
  *
  */
 module.exports = {
   link: [
     /** <link> tags for 'apple-touch-icon' (AKA Web Clips). **/
-    { rel: 'apple-touch-icon', sizes: '57x57', href: 'assets/icon/apple-icon-57x57.png' },
-    { rel: 'apple-touch-icon', sizes: '60x60', href: 'assets/icon/apple-icon-60x60.png' },
-    { rel: 'apple-touch-icon', sizes: '72x72', href: 'assets/icon/apple-icon-72x72.png' },
-    { rel: 'apple-touch-icon', sizes: '76x76', href: 'assets/icon/apple-icon-76x76.png' },
-    { rel: 'apple-touch-icon', sizes: '114x114', href: 'assets/icon/apple-icon-114x114.png' },
-    { rel: 'apple-touch-icon', sizes: '120x120', href: 'assets/icon/apple-icon-120x120.png' },
-    { rel: 'apple-touch-icon', sizes: '144x144', href: 'assets/icon/apple-icon-144x144.png' },
-    { rel: 'apple-touch-icon', sizes: '152x152', href: 'assets/icon/apple-icon-152x152.png' },
-    { rel: 'apple-touch-icon', sizes: '180x180', href: 'assets/icon/apple-icon-180x180.png' },
+    { rel: 'apple-touch-icon', sizes: '57x57', href: 'assets/icon/apple-touch-icon-57x57.png' },
+    { rel: 'apple-touch-icon', sizes: '60x60', href: 'assets/icon/apple-touch-icon-60x60.png' },
+    { rel: 'apple-touch-icon', sizes: '72x72', href: 'assets/icon/apple-touch-icon-72x72.png' },
+    { rel: 'apple-touch-icon', sizes: '76x76', href: 'assets/icon/apple-touch-icon-76x76.png' },
+    { rel: 'apple-touch-icon', sizes: '114x114', href: 'assets/icon/apple-touch-icon-114x114.png' },
+    { rel: 'apple-touch-icon', sizes: '120x120', href: 'assets/icon/apple-touch-icon-120x120.png' },
+    { rel: 'apple-touch-icon', sizes: '144x144', href: 'assets/icon/apple-touch-icon-144x144.png' },
+    { rel: 'apple-touch-icon', sizes: '152x152', href: 'assets/icon/apple-touch-icon-152x152.png' },,
 
-    /** <link> tags for android web app icons **/
-    { rel: 'icon', type: 'image/png', sizes: '192x192', href: 'assets/icon/android-icon-192x192.png' },
+    { rel: 'icon', type: 'image/png', sizes: '196x196', href: 'assets/icon/favicon-196x196.png' },
 
     /** <link> tags for favicons **/
     { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'assets/icon/favicon-32x32.png' },
@@ -38,8 +36,8 @@ module.exports = {
     { rel: 'manifest', href: 'assets/manifest.json' }
   ],
   meta: [
-    { name: 'msapplication-TileColor', content: '#00bcd4' },
+    { name: 'msapplication-TileColor', content: '#3feca5' },
     { name: 'msapplication-TileImage', content: 'assets/icon/ms-icon-144x144.png', '=content': true },
-    { name: 'theme-color', content: '#00bcd4' }
+    { name: 'theme-color', content: '#3feca5' }
   ]
 };
